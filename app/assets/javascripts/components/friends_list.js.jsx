c0a991fd609904852ecc9f68b7fa4c144a67aea2
Ben Whittle <benwhittle31@gmail.com>
2016-04-10 18:02:09 -0700

    fixing trailing whitespace problem and giving each <li> tag a key attribute

--- app/assets/javascripts/components/friends_list.js.jsx
+++ app/assets/javascripts/components/friends_list.js.jsx
@@ -2,7 +2,7 @@ var FriendsList = React.createClass({
 
   render: function() {
     var listItems = this.props.names.map(function (name) {
-      return <li> {name} </li>;
+      return <li key={name}>{name}</li>;
     });
 
     return (
